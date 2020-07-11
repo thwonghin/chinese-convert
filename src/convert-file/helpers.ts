e92ebc85fab745bca295971317ca9ee0fc6ad9aa
Hin Wong <thwong.henry@gmail.com>
2020-07-11 21:00:50 +0800

    refactor: extract utils functions

---
+++ src/convert-file/helpers.ts
@@ -0,0 +1,36 @@
+import * as path from 'path';
+
+import { isFile, isFilePathExist } from '../utils';
+
+interface ResolveOutPathParams {
+    inPath: string;
+    outPath: string;
+    shouldReplace: boolean;
+}
+
+export async function resolveOutPath({
+    inPath,
+    outPath,
+    shouldReplace,
+}: ResolveOutPathParams): Promise<string> {
+    if (shouldReplace) {
+        return inPath;
+    }
+
+    let result = outPath;
+
+    if (!(await isFilePathExist(outPath)) || !(await isFile(outPath))) {
+        result = path.resolve(`${outPath}`, path.basename(inPath));
+    }
+
+    if (inPath === result) {
+        const parsed = path.parse(result);
+        return path.format({
+            ...parsed,
+            name: `${parsed.name}-new`,
+            base: undefined,
+        });
+    }
+
+    return result;
+}
